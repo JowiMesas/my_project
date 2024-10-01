6b3abfc4a676e8dd7e173582572394289c2bd9a0
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-10-01 17:03:37 +0200

    filtrarNombre

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -3,16 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class NurseController extends AbstractController
 {
-    #[Route('/nurse', name: 'app_nurse')]
-    public function findByName(): Response
+    #[Route('/nurse', name: 'app_nurse', methods:['GET'])]
+    public function findByName(Request $peticionNurse): JsonResponse
     {
+        $nameNurse = $peticionNurse ->query -> get('first_name');
         $json_nurse = file_get_contents('C:\Users\JOELMESASHONTORIA\Documents\Symfony\my_project\src\Controller\DATA.json');
         $json_data = json_decode($json_nurse, true);
-        return new Response('<pre>' . print_r($json_data, true) . '</pre>');
+        $filtrarNombre = array_filter($json_data, function($nurse) use ($nameNurse){
+            return strtolower($nurse['first_name']) === strtolower($nameNurse);
+        });
+        return new JsonResponse(array_values($filtrarNombre));
     }
 }
