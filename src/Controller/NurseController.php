09523fc8aa0e498f06230de81e7182f291e80ec7
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-10-01 16:03:32 +0200

    symfony

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -6,14 +6,4 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-class NurseController extends AbstractController
-{
-    #[Route('/nurse', name: 'app_nurse')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/NurseController.php',
-        ]);
-    }
-}
+
