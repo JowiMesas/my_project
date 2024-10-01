f55ca9ee2db2e588f1ac15ef3f342c2332059d56
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-10-01 16:04:18 +0200

    symfony

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -3,7 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
+class NurseController extends AbstractController
+{
+    #[Route('/nurse', name: 'app_nurse')]
+    public function findByName(): Response
+    {
+        $json_nurse = file_get_contents('C:\Users\JOELMESASHONTORIA\Documents\Symfony\my_project\src\Controller\DATA.json');
+        $json_data = json_decode($json_nurse, true);
+        return new Response('<pre>' . print_r($json_data, true) . '</pre>');
+    }
+}
