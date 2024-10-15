39ed695abee7b443f76601ae70f3d1325f8ed067
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-10-15 15:42:20 +0200

    individual

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -6,18 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class NurseController extends AbstractController
 {
-    #[Route('/nurse', name: 'app_nurse', methods:['GET'])]
-    public function findByName(Request $peticionNurse): JsonResponse
+    #[Route('/findName', name: 'app_nurse_findName')]
+    public function findName(Request $peticionNurse): JsonResponse
     {
-        $nameNurse = $peticionNurse ->query -> get('first_name');
+        $nameNurse = $peticionNurse->query->get('first_name');
         $json_nurse = file_get_contents('DATA.json');
         $json_data = json_decode($json_nurse, associative: true);
-        $filtrarNombre = array_filter($json_data, function($nurse) use ($nameNurse){
+        $filtrarNombre = array_filter($json_data, function ($nurse) use ($nameNurse) {
             return strtolower($nurse['first_name']) === strtolower($nameNurse);
         });
-        return new JsonResponse(array_values($filtrarNombre));
+        if (!empty($filtrarNombre)) {
+            // Retornar los resultados y el código de estado 200
+            return new JsonResponse(array_values($filtrarNombre), Response::HTTP_OK);
+        } else {
+            // Si no se encuentra el nombre, retornar 404 con un mensaje
+            return new JsonResponse(['message' => 'El enfermero con ese nombre no existe.'], Response::HTTP_NOT_FOUND);
+        }
     }
 }
